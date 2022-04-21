597c3986fe90266c7ea217cd29bbbc4a94533ac9
IsraelDBraga <israeldbraga1@gmail.com>
2022-04-21 10:21:10 -0300

    .

---
+++ React/src/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDom from "react-dom"
+import "./style.css"
+
+
+const App = () => {
+
+    return (
+        <div className="App">
+        Hello World
+        </div>
+
+    )
+
+}
+
+const rootElement = document.getElementById("root")
+ReactDom.render(<App />, rootElement)
